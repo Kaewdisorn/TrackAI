6654321dee65afcf5ec9e92fa05ab1f5358d66d2
Nane <kds.napha@gmail.com>
2025-11-17 07:09:36 +0900

    auto complete widget added

---
+++ frontend/simulation/lib/widgets/location_search_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LocationSearchField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final List<String> options;
+
+  /// A reusable Location Search Field
+  /// [controller]: the TextEditingController to read/write value
+  /// [labelText]: text shown in the TextField label
+  /// [options]: list of autocomplete suggestions (can be replaced with API later)
+  const LocationSearchField({super.key, required this.controller, required this.labelText, required this.options});
+
+  @override
+  Widget build(BuildContext context) {
+    return Autocomplete<String>(
+      optionsBuilder: (textEditingValue) {
+        if (textEditingValue.text.isEmpty) return const Iterable<String>.empty();
+        return options.where((option) => option.toLowerCase().contains(textEditingValue.text.toLowerCase()));
+      },
+      onSelected: (selection) {
+        controller.text = selection;
+      },
+      fieldViewBuilder: (context, textEditingController, focusNode, onFieldSubmitted) {
+        // Sync internal controller with external
+        textEditingController.addListener(() {
+          controller.text = textEditingController.text;
+        });
+
+        return TextField(
+          controller: textEditingController, // <-- use internal controller
+          focusNode: focusNode,
+          decoration: InputDecoration(labelText: labelText, border: const OutlineInputBorder()),
+        );
+      },
+    );
+  }
+}
