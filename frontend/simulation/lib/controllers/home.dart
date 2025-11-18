f1a3cbd6ba776590b2bb08ccc2f3182b734f4642
Nane <kds.napha@gmail.com>
2025-11-19 01:10:47 +0900

    added clear button

--- frontend/simulation/lib/controllers/home.dart
+++ frontend/simulation/lib/controllers/home.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 
 class HomeController {
+  final TextEditingController startController = TextEditingController();
+  final TextEditingController endController = TextEditingController();
+
   final List<String> sampleLocations = ['Seoul', 'Busan', 'Incheon', 'Daejeon', 'Daegu'];
   LatLng? startLatLng;
   LatLng? endLatLng;
@@ -10,6 +14,14 @@ class HomeController {
     startLatLng = LatLng(37.5665, 126.9780); // Seoul
     endLatLng = LatLng(35.1796, 129.0756); // Busan
     routePoints = generateRoute(startLatLng!, endLatLng!);
+  }
+
+  void clearRoute() {
+    startController.clear();
+    endController.clear();
+    startLatLng = null;
+    endLatLng = null;
+    routePoints = [];
   }
 
   List<LatLng> generateRoute(LatLng start, LatLng end, {int segments = 20}) {
