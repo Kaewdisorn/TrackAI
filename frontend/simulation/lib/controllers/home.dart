652d02b15f313104ff33a9f3cd36179730dd314a
Nane <kds.napha@gmail.com>
2025-11-19 05:22:25 +0900

    added simulState

--- frontend/simulation/lib/controllers/home.dart
+++ frontend/simulation/lib/controllers/home.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 
+import '../widgets/map.dart';
+
+enum SimulationState { initial, running, paused }
+
 class HomeController {
   final TextEditingController startController = TextEditingController();
   final TextEditingController endController = TextEditingController();
   final ValueNotifier<List<LatLng>> routePointsNotifier = ValueNotifier([]);
+  final GlobalKey<RouteMapState> mapKey = GlobalKey<RouteMapState>();
+  final simulationState = ValueNotifier<SimulationState>(SimulationState.initial);
 
   final List<String> sampleLocations = ['Seoul', 'Busan', 'Incheon', 'Daejeon', 'Daegu'];
   LatLng? startLatLng;
   LatLng? endLatLng;
   List<LatLng> routePoints = [];
+
+  void startSimulation() {
+    if (mapKey.currentState != null && routePointsNotifier.value.isNotEmpty) {
+      mapKey.currentState!.startSimulation(routePointsNotifier.value);
+      simulationState.value = SimulationState.running;
+    }
+  }
+
+  void pauseSimulation() {
+    if (mapKey.currentState != null) {
+      mapKey.currentState!.pauseSimulation();
+      simulationState.value = SimulationState.paused;
+    }
+  }
+
+  void resumeSimulation() {
+    if (mapKey.currentState != null) {
+      mapKey.currentState!.resumeSimulation();
+      simulationState.value = SimulationState.running;
+    }
+  }
 
   void setRoute() {
     startLatLng = LatLng(37.5665, 126.9780); // Seoul
